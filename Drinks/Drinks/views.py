ed00fd066c3584759e4171db444eac9ad91d1673
Bayan  Alallan <youremail@example.com>
2024-07-08 13:34:45 -0700

    another commit

--- Drinks/Drinks/views.py
+++ Drinks/Drinks/views.py
@@ -1,13 +1,13 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def index (request):
     return HttpResponse("Hello")
 
 
-
-
-
-
+def Drinks(request):
+    return render(request, 'Drinks/drinks.html', {'data': 'test-data'})
 
 
 
